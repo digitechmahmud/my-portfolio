3a651e101a967555983c88f6deb8d66a4859938d
digitechmahmud <mdmahmudulhassanbd@gmail.com>
2023-02-01 23:46:50 +0600

    Create Expertise Section

--- src/pages/Home/MyExpertise/MyExpertise.js
+++ src/pages/Home/MyExpertise/MyExpertise.js
@@ -1,34 +1,138 @@
 import React from 'react';
 import { SiManjaro } from "react-icons/si";
+import { HiOutlineCode } from "react-icons/hi";
 
 const MyExpertise = () => {
     return (
         <div className='min-h-screen mx-36'>
             <h2 className='font-bold text-3xl  mt-10 text-slate-300 underline'>My Expertise</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 mt-20 text-slate-300'>
-                <div className='hover:text-slate-500 border-2'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 mt-20 text-slate-300'>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
                     <div>
                         <div className=' flex p-5'>
-                            <p className='font-semibold text-2xl p-3 pt-5'><SiManjaro /></p>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
                             <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
                         </div>
-                        <div>
-                            <p>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
                         </div>
                     </div>
                 </div>
-                <div className='hover:text-slate-500'>
-                    <div className='border-2 flex p-5'>
-                        <p className='font-semibold text-2xl p-3 pt-5'><SiManjaro /></p>
-                        <h3 className='font-semibold text-2xl'>Frontend<br/> Development</h3>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
                     </div>
                 </div>
-                <div className='hover:text-slate-500'>
-                    <div className='border-2 flex p-5'>
-                        <p className='font-semibold text-2xl p-3 pt-5'><SiManjaro /></p>
-                        <h3 className='font-semibold text-2xl'>Frontend<br/> Development</h3>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
                     </div>
                 </div>
+                
+                
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 mt-20 text-slate-300'>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
+                    </div>
+                </div>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
+                    </div>
+                </div>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
+                    </div>
+                </div>
+                
+                
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-6 mt-20 text-slate-300'>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
+                    </div>
+                </div>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
+                    </div>
+                </div>
+                <div className='hover:text-slate-500 border-2 hover:border-slate-500 py-2'>
+                    <div>
+                        <div className=' flex p-5'>
+                            <p className='font-semibold text-2xl p-3'><SiManjaro /></p>
+                            <h3 className='font-semibold text-2xl'>Frontend<br /> Development</h3>
+                        </div>
+                        <div className='mx-8 content-justify'>
+                            <HiOutlineCode className='text-slate-500 text-3xl mb-2'/>
+                            <p className='text-xl text-justify'>More than 3 years of experience in React.js, Next.js, JS, HTML5, CSS3, SASS, and Bootstrap5.</p>
+                            <HiOutlineCode className='text-slate-500 text-3xl mt-2' />
+                        </div>
+                    </div>
+                </div>
+                
                 
             </div>
         </div>
